82035892b850abc5584735d148504d0591fdc05e
Raymond_Iacobacci <rliacobacci@ucdavis.edu>
2025-01-02 16:13:15 -0800

    Crucial updates to calculation ordering

--- solver_v2.py
+++ solver_v2.py
@@ -115,39 +115,55 @@ class Solver:
         vac_p, vac_q = self.pq_matrices(self.layer_stack[0], self.kx0)
         vac_lambda_w_sqr, vac_w = np.linalg.eig(vac_p @ vac_q)
         vac_lambda_w = np.sqrt(vac_lambda_w_sqr)
+        def generate_random_ordering(size):
+            from random import shuffle
+            """
+            Generates a random ordering of integers from 0 to size-1.
+
+            :param size: The upper limit (exclusive) for the range of integers.
+            :return: A list containing the integers 0 through size-1 in random order.
+            """
+            numbers = list(range(size))  # Create a list of integers from 0 to size-1
+            shuffle(numbers)     # Shuffle the list in place
+            return numbers
+        reorder = generate_random_ordering(2*self.graph_harmonics)
+        # vac_lambda_w = vac_lambda_w[reorder]
         vac_fwd_lambda_w = np.where(np.real(vac_lambda_w) < 0, -vac_lambda_w, vac_lambda_w)
         vac_bwd_lambda_w = np.where(np.real(vac_lambda_w) >= 0, -vac_lambda_w, vac_lambda_w)
         vac_trns_matrix = np.block([[self.id_block('double'), self.id_block('double')], [vac_q @ np.linalg.inv(np.diag(vac_bwd_lambda_w)) * -1j, vac_q @ np.linalg.inv(np.diag(vac_fwd_lambda_w)) * -1j]]) # NOTE: sign convention
         
         # vac_v = vac_q @ vac_w @ np.linalg.inv(np.diag(vac_lambda_w))
         # vac_trns_matrix = np.block([[vac_w, vac_w], [-vac_v, vac_v]])
-        print('Vacuum transfer matrix:')
-        ff.printdf(vac_trns_matrix)
+        # print('Vacuum transfer matrix:')
+        # ff.printdf(vac_trns_matrix)
 
         for i, layer in enumerate(self.layer_stack[1:]):
             p, q = self.pq_matrices(layer, self.kx0)
             
             lambda_w_sqr, w = np.linalg.eig(p @ q) # Eigenvectors, Eigenvalues pairings are unique to each matrix, and the Eigenvectors of a matrix are the Eigenvectors of the square of the matrix
+            print(f'Lambda before:\n{lambda_w_sqr}')
+            w, lambda_w_sqr, res_decomp, res_evals = ff.custom_eigendecomposition(p @ q)
+            lambda_w_sqr = [lambda_w_sqr[itr, itr] for itr in range(lambda_w_sqr.shape[0])]
+            print(f'Lambda after:\n{lambda_w_sqr}')
             lambda_w = np.sqrt(lambda_w_sqr)
-
+            # lambda_w = lambda_w[reorder]
+            # print("Eigenvalues:")
+            # for i in range(lambda_w.shape[0]):
+            #     print(i, lambda_w[i])
+            # break
             fwd_lambda_w = np.where(np.real(lambda_w) < 0, -lambda_w, lambda_w)
             fwd_omega = w @ np.diag(fwd_lambda_w) @ np.linalg.inv(w)
-            # print(f'Forward omega:\n{fwd_omega}')
             fwd_prop_matrix = sp.linalg.expm(fwd_omega * -layer.thickness)
 
             bwd_lambda_w = np.where(np.real(lambda_w) >= 0, -lambda_w, lambda_w) #removed the = sign
             
             bwd_omega = w @ np.diag(bwd_lambda_w) @ np.linalg.inv(w)
             bwd_prop_matrix = sp.linalg.expm(bwd_omega * layer.thickness)
-            
             trns_matrix = np.block([[self.id_block('double'), self.id_block('double')], [q @ np.linalg.inv(np.diag(bwd_lambda_w)) * -1j, q @ np.linalg.inv(np.diag(fwd_lambda_w)) * -1j]])
-            ff.printdf((trns_matrix))
-            ff.printdf(np.linalg.inv(trns_matrix))
             M1 = np.linalg.inv(trns_matrix) @ vac_trns_matrix
             m11, m12, m21, m22 = ff.quar(M1)
-            # print(M1)
-            # print(m11, m12, m21, m22)
             fref_coefs = -np.linalg.inv(m22) @ m21
+            print(np.sum(np.abs(fref_coefs)))
             for i in range(fref_coefs.shape[0]):
                 print(i, fref_coefs[i,i])
             # print(fref_coefs)
